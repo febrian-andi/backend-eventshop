a961be57cd6f80f51a6eb91da8cd8943893f56a0
Febrian Andi Nugroho <febrianandinugroho@gmail.com>
2025-05-13 00:54:57 +0700

    fix(swagger): define SwaggerUIBundle to fix UI initialization error

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -1,21 +1,11 @@
 import { Express } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerOutput from "./swagger_output.json";
-import fs from "fs";
-import path from "path";
 
 export default function docs(app: Express) {
-  const css = fs.readFileSync(
-    path.resolve(__dirname, "./swagger-ui.css"),
-    "utf8"
-  );
-  
   app.use(
     "/api-docs",
     swaggerUi.serve,
-    swaggerUi.setup(swaggerOutput, {
-      customCss: css,
-      // customCssUrl: "https://unpkg.com/swagger-ui-dist/swagger-ui.css"
-    })
+    swaggerUi.setup(swaggerOutput)
   );
 }
