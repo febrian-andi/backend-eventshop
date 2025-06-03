8d88b1abcd009d86b0178d32e594645f5a3d9fda
Febrian Andi Nugroho <febrianandinugroho@gmail.com>
2025-06-03 23:27:43 +0700

    feat: implement GET API to retrieve region data

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,12 +2,13 @@ import express from "express";
 import authRoutes from "./auth.routes";
 import mediaRoutes from "./media.routes";
 import categoryRoutes from "./category.routes";
-
+import regionRoutes from "./region.routes";
 
 const router = express.Router();
 
 router.use(authRoutes)
 router.use(mediaRoutes);
 router.use(categoryRoutes);
+router.use(regionRoutes);
 
 export default router;
--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import mongoose from "mongoose";
 import * as Yup from "yup";
 
 type Pagination = {
@@ -25,7 +26,30 @@ export default {
           status: 400,
           message,
         },
-        data: error.errors,
+        data:{
+          [`${error.path}`]: error.errors[0],
+        },
+      });
+    }
+
+    if (error instanceof mongoose.Error) {
+      return res.status(500).json({
+        meta: {
+          status: 500,
+          message: error.message,
+        },
+        data: error.name,
+      });
+    }
+
+    if ((error as any)?.code) {
+      const _err = error as any;
+      return res.status(500).json({
+        meta: {
+          status: 500,
+          message: _err.errorResponse.errmsg,
+        },
+        data: _err,
       });
     }
 
@@ -34,7 +58,7 @@ export default {
         status: 500,
         message,
       },
-      data: null,
+      data: error,
     });
   },
 
